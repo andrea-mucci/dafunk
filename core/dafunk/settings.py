58fe7b0e753e96256440a3a6fcdcc592f31ea2b4
Andrea Mucci <andrea@clariteia.com>
2025-01-22 22:20:25 +0100

    added staging variables for settings
    modified and added tests

--- core/dafunk/settings.py
+++ core/dafunk/settings.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Type, Self
+from typing import TypeVar, Type, Self, Optional
 
 import yaml
 from pydantic import BaseModel, Field
@@ -6,15 +6,23 @@ import os
 
 from core.dafunk.utils import dict_keys_lower
 
-
 class BaseSettings(BaseModel):
-    db_url: str = Field(..., description="The database url",
+    db_url: str = Field(description="The database url",
                         examples=["postgresql+psycopg://scott:tiger@localhost/test",])
-    broker_url: str = Field(..., description="Event Broker url, we support NATS, RabbitMQ, Kafka, Redis")
+    broker_url: str = Field(description="Event Broker url, we support NATS, RabbitMQ, Kafka, Redis")
     logger: bool = Field(False, description="Logger status active or inactive")
-    logger_url: str = Field(..., description="Internal logger url")
+    logger_url: str = Field(description="Internal logger url")
+
+class StagingSettings(BaseModel):
+    default: Optional[BaseSettings]
+    test: Optional[BaseSettings]
+    dev: Optional[BaseSettings]
+    stag: Optional[BaseSettings]
+    prod: Optional[BaseSettings]
+
 
 TBaseSetting = TypeVar("TBaseSetting", bound=BaseSettings)
+TStagingSetting = TypeVar("TStagingSetting", bound=StagingSettings)
 
 class DaSettings(object):
     _prefix: str = "DAFUNK_"
@@ -25,12 +33,27 @@ class DaSettings(object):
             self._settings = yaml.load(stream, Loader=yaml.FullLoader)
             self._object_model = None
         # merge settings with env variables
+
         dict_env_variables = self._load_environment_variables()
         dict_env_variables = dict_keys_lower(dict_env_variables)
+        stage = None
+        if "staging" in dict_env_variables:
+            stage = dict_env_variables["staging"]
+            del dict_env_variables["staging"]
+        self._settings = self._parse_stages(stage)
         if isinstance(self._settings, dict) and isinstance(dict_env_variables, dict):
             self._settings.update(
                 dict_env_variables
             )
+
+    def _parse_stages(self, stage=None) -> dict:
+        if "default" in self._settings:
+            settings = self._settings['default']
+        else:
+            settings = {}
+        if stage is not None and stage in self._settings:
+            settings.update(self._settings[stage])
+        return settings
 
     def _load_environment_variables(self, prefix: str = None) -> dict:
         new_dict = {}
--- core/tests/test_dafunk_core_library.py
+++ core/tests/test_dafunk_core_library.py
@@ -8,16 +8,6 @@ from core.dafunk import DaSettings
 actual_path = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_settings_class():
-    settings_file = os.path.join(actual_path, "fixtures", "settings.yaml")
-    object_settings = DaSettings.load_from_file(settings_file)
-    assert object_settings.settings == {
-        'db_url': 'postgresql+psycopg://scott:tiger@localhost/test',
-        'broker_url': 'nats://localhost:4222',
-        'logger': False,
-        'logger_url': 'https://localhost:4034'}
-
-
 def test_settings_class_merge_env(monkeypatch):
     monkeypatch.setenv("DAFUNK_DB_URL",
                        "postgresql+psycopg://scott:tiger@localhost/tester")
@@ -40,4 +30,48 @@ def test_settings_class_merge_multiple_env(monkeypatch):
         'db_url': 'postgresql+psycopg://scott:tiger@localhost/tester',
         'broker_url': 'nats://localhost:4222',
         'logger': True,
+        'logger_url': 'https://localhost:4034'}
+
+
+def test_settings_class_test_staging(monkeypatch):
+    monkeypatch.setenv("DAFUNK_STAGING", "test")
+    settings_file = os.path.join(actual_path, "fixtures", "settings.yaml")
+    object_settings = DaSettings.load_from_file(settings_file)
+    assert object_settings.settings == {
+        'db_url': 'sqlite:///test_db.db',
+        'broker_url': 'nats://localhost:4222',
+        'logger': False,
+        'logger_url': 'https://localhost:4034'}
+
+
+def test_settings_class_dev_staging(monkeypatch):
+    monkeypatch.setenv("DAFUNK_STAGING", "dev")
+    settings_file = os.path.join(actual_path, "fixtures", "settings.yaml")
+    object_settings = DaSettings.load_from_file(settings_file)
+    assert object_settings.settings == {
+        'db_url': 'postgresql+psycopg://scott:tiger@dev.example.com/test',
+        'broker_url': 'nats://dev.example.com:4222',
+        'logger': False,
+        'logger_url': 'https://localhost:4034'}
+
+
+def test_settings_class_stag_staging(monkeypatch):
+    monkeypatch.setenv("DAFUNK_STAGING", "stag")
+    settings_file = os.path.join(actual_path, "fixtures", "settings.yaml")
+    object_settings = DaSettings.load_from_file(settings_file)
+    assert object_settings.settings == {
+        'db_url': 'postgresql+psycopg://john:doe@stag.example.com/test',
+        'broker_url': 'nats://stag.example.com:4222',
+        'logger': False,
+        'logger_url': 'https://localhost:4034'}
+
+
+def test_settings_class_prod_staging(monkeypatch):
+    monkeypatch.setenv("DAFUNK_STAGING", "prod")
+    settings_file = os.path.join(actual_path, "fixtures", "settings.yaml")
+    object_settings = DaSettings.load_from_file(settings_file)
+    assert object_settings.settings == {
+        'db_url': 'postgresql+psycopg://scott:tiger@prod.example.com/test',
+        'broker_url': 'nats://prod.example.com:4222',
+        'logger': False,
         'logger_url': 'https://localhost:4034'}
